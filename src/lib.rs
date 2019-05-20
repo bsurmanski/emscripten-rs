4636c0159d05411f80f5d29fa8fc49d220e535ed
Brandon Surmanski <b.surmanski@gmail.com>
2019-05-19 23:56:15 -0400

    use 'extern "C"' for the callback fn type

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::os::raw::{c_int, c_char, c_void, c_float};
 use std::ffi::CString;
 
 #[allow(non_camel_case_types)]
-type em_callback_func = unsafe extern fn();
+type em_callback_func = unsafe extern "C" fn();
 
 extern {
     pub fn emscripten_set_main_loop(func: em_callback_func, fps: c_int, simulate_infinite_loop: c_int);
